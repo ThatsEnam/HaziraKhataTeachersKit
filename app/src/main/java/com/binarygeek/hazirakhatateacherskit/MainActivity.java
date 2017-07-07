e872043eff82ce4822b48f81bc5e619cc219a688
Thats Enam <you@example.com>
2017-07-07 18:03:53 +0600

    Minor Edit 1

--- app/src/main/java/com/binarygeek/hazirakhatateacherskit/MainActivity.java
+++ app/src/main/java/com/binarygeek/hazirakhatateacherskit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //code from gk
         //hello from Enam
+        //hello again
     }
 }
