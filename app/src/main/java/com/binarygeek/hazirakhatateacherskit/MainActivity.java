6dd92e307f819d0e680f8d10f15abf81780c692a
Gkemon <emon.info2013@gmail.com>
2017-07-12 00:12:34 +0600

    Added By GK

--- app/src/main/java/com/binarygeek/hazirakhatateacherskit/MainActivity.java
+++ app/src/main/java/com/binarygeek/hazirakhatateacherskit/MainActivity.java
@@ -1,17 +1,31 @@
 package com.binarygeek.hazirakhatateacherskit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //code from gk
-        //hello from Enam
-        //hello again
-        //code from gk1
+        context = this;
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionBar);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ClassActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
